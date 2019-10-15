adde4393ece3f9b89c1035fb2d3803dd64670959
jzara3115 <jzara3115@gmail.com>
2019-10-15 12:58:46 -0700

    Script change

---
+++ java.js
@@ -0,0 +1,6 @@
+//alert("work");//
+function buttonclick() {
+     document.getElementById('read').innerHTML = 'I dont know if it worked';
+     document.getElementById('button').style.display="none";
+};
+
--- script.js
+++
@@ -1,4 +0,0 @@
-//alert("work");//
-function buttonclick() {
-document.getElementById('read').innerHTML = 'I dont Know if it worked';
-}
